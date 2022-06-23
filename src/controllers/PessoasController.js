7497e6cfd31c482f99a5c12c8db1794f113dff26
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-23 17:53:52 -0300

    create crud controller

--- src/controllers/PessoasController.js
+++ src/controllers/PessoasController.js
@@ -13,7 +13,43 @@ class PessoasController {
 
       try {
          const umaPessoa = await db.Pessoas.findOne({ where: { id } });
-         return res.status(200).json(umaPessoa)
+         return res.status(200).json(umaPessoa);
+      } catch (error) {
+         return res.status(500).json(error.message);
+      }
+   }
+
+   static async criaPessoa(req, res) {
+      const novaPessoa = req.body;
+
+      try {
+         const novaPessoaCriada = await db.Pessoas.create(novaPessoa);
+         return res.status(201).json(novaPessoaCriada);
+      } catch (error) {
+         return res.status(500).json(error.message);
+      }
+   }
+
+   static async atualizaPessoa(req, res) {
+      const { id } = req.params;
+      const novasInfos = req.body;
+
+      try {
+         await db.Pessoas.update(novasInfos, { where: { id } });
+
+         const pessoaAtualizada = await db.Pessoas.findOne({ where: { id } });
+
+         return res.status(200).json(pessoaAtualizada);
+      } catch (error) {
+         return res.status(500).json(error.message);
+      }
+   }
+
+   static async apagaPessoa(req, res) {
+      const { id } = req.params;
+      try {
+         await db.Pessoas.destroy({ where: { id } });
+         res.status(204).json({message: `id ${id} deletado!`});
       } catch (error) {
          return res.status(500).json(error.message);
       }
--- src/routes/pessoasRoutes.js
+++ src/routes/pessoasRoutes.js
@@ -3,7 +3,10 @@ const PessoasController = require("../controllers/PessoasController");
 
 const routes = Router();
 
-routes.get('/pessoas', PessoasController.pegaTodasAsPessoas)
-routes.get('/pessoas/:id', PessoasController.pegarUmaPessoa)
+routes.get("/pessoas", PessoasController.pegaTodasAsPessoas);
+routes.get("/pessoas/:id", PessoasController.pegarUmaPessoa);
+routes.post("/pessoas", PessoasController.criaPessoa);
+routes.put("/pessoas/:id", PessoasController.atualizaPessoa);
+routes.delete("/pessoas/", PessoasController.apagaPessoa);
 
 module.exports = routes;
