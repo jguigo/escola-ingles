b44988565ca8fa301206d4a3331031506b43941d
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-24 18:17:50 -0300

    adicionando chaves estrangeiras nas migrates!

--- src/database/migrations/20220624191903-create-turmas.js
+++ src/database/migrations/20220624191903-create-turmas.js
@@ -1,27 +1,43 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Turmas', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      data_inicio: {
-        type: Sequelize.DATEONLY
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Turmas');
-  }
+   async up(queryInterface, Sequelize) {
+      await queryInterface.createTable("Turmas", {
+         id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+         },
+         data_inicio: {
+            type: Sequelize.DATEONLY,
+         },
+         estudante_id: {
+            allowNull: false,
+            type: Sequelize.INTEGER,
+            references: {
+               model: "Pessoas",
+               key: "id",
+            },
+         },
+         turma_id: {
+            allowNull: false,
+            type: Sequelize.INTEGER,
+            references: {
+               model: "Turmas",
+               key: "id",
+            },
+         },
+         createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+         updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+      });
+   },
+   async down(queryInterface, Sequelize) {
+      await queryInterface.dropTable("Turmas");
+   },
 };
--- src/database/migrations/20220624194018-create-matriculas.js
+++ src/database/migrations/20220624194018-create-matriculas.js
@@ -1,27 +1,43 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Matriculas', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      status: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Matriculas');
-  }
+   async up(queryInterface, Sequelize) {
+      await queryInterface.createTable("Matriculas", {
+         id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+         },
+         status: {
+            type: Sequelize.STRING,
+         },
+         docente_id: {
+            allowNull: false,
+            type: Sequelize.INTEGER,
+            references: {
+               model: "Estudante",
+               key: "id",
+            },
+         },
+         docente_id: {
+            allowNull: false,
+            type: Sequelize.INTEGER,
+            references: {
+               model: "Niveis",
+               key: "id",
+            },
+         },
+         createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+         updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+         },
+      });
+   },
+   async down(queryInterface, Sequelize) {
+      await queryInterface.dropTable("Matriculas");
+   },
 };
