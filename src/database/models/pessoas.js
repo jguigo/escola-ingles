65d9f8c21f98f14e8e8ad3d953e13e5c21895e82
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-28 17:40:07 -0300

    adicionando defaultScope e scopes para modelo pessoas

--- src/database/models/pessoas.js
+++ src/database/models/pessoas.js
@@ -28,6 +28,14 @@ module.exports = (sequelize, DataTypes) => {
          sequelize,
          modelName: "Pessoas",
          paranoid: true,
+         defaultScope: {
+            where: {
+               ativo: true,
+            },
+         },
+         scopes: {
+            todos: { where: {} },
+         },
       }
    );
    return Pessoas;
