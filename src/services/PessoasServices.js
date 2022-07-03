61308ef11e0f44e0656289bd7a8840766e6ae14b
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-07-03 20:41:52 -0300

    adicionando o metodo de cancela pessoa matriculas

--- src/services/PessoasServices.js
+++ src/services/PessoasServices.js
@@ -4,6 +4,7 @@ const database = require("../database/models");
 class PessoasServices extends Services {
    constructor() {
       super("Pessoas");
+      this.matriculas = new Services("Matriculas");
    }
 
    async pegaRegistrosAtivos(where = {}) {
@@ -13,8 +14,22 @@ class PessoasServices extends Services {
    async pegaTodosOsRegistros(where = {}) {
       //.scope("todos") --> foi definido no modelo de Pessoas, e para utiliza-lo, coloco antes do método que quero utilizar
       return database[this.nomeDoModelo]
-      .scope("todos")
-      .findAll({ where: { ...where } });
+         .scope("todos")
+         .findAll({ where: { ...where } });
+   }
+
+   async cancelaPessoaMatriculas(estudanteId) {
+      //com o uso da transaction, qualquer umas das operação com o banco que der problema, ele roda um rollback
+      return database.sequelize.transaction(async (transacao) => {
+         await super.atualizaRegistro({ ativo: false }, estudanteId, {
+            transaction: transacao,
+         });
+         await this.matriculas.atualizaRegistros(
+            { status: "cancelado" },
+            { estudante_id: estudanteId },
+            { transaction: transacao }
+         );
+      });
    }
 }
 
